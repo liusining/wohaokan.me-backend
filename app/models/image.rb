044e645788ef88c9000fc0307e1436e9b8d3285c
liusining <siningdream@gmail.com>
2019-01-01 20:08:25 +0800

    Fix a typo

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,7 +2,7 @@ class Image < ApplicationRecord
   belongs_to :user
   enum gender: [:Female, :Male]
 
-  def assign_to_user
+  def assign_to_user!
     self.user.current_image = img
     self.user.save!
   end
