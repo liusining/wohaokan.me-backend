a751ad2aa4f29e02f80c10868cb02036f1d61852
liusining <siningdream@gmail.com>
2019-01-03 15:32:38 +0800

    Fix: use string as amount to keep from float number issues

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,7 +4,7 @@ class Order < ApplicationRecord
 
   WOHAOKANME_CLIENT_ID = ENV['WOHAOKANME_CLIENT_ID'] || Rails.application.secrets['wohaokanme_client_id']
   EOS_ASSET_ID = ENV['EOS_ASSET_ID'] || Rails.application.secrets['eos_asset_id']
-  AMOUNT = 0.1
+  AMOUNT = '0.1'
 
   def mixin_pay_url
     "https://mixin.one/pay?recipient=#{WOHAOKANME_CLIENT_ID}&asset=#{EOS_ASSET_ID}&amount=#{AMOUNT}&trace=#{trace_id}&memo=打赏会全部转发给 Ta"
